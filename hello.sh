285c681d04b3005e72a03afc022687e797a1a8de
Brian Yang <brianyang1106@gmail.com>
2014-01-20 01:29:35 -0600

    general test 0

--- hello.sh
+++ hello.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-
+echo
+############################################
 ps | grep $$
 
 which bash
 
 echo 'O hai, world!'
+
+greeting='Hello     world!'
+echo $greeting now with spaces: "$greeting"
+
+BIRTHDATE="Jan 1 2000"
+Presents=10
+BIRTHDAY=`date -d "$BIRTHDATE" +%A`
+
+echo $BIRTHDAY
+############################################
+echo
