91e06d759ad4fc6e1bb08e4cf0638a3adebd6310
Brian Yang <brianyang1106@gmail.com>
2014-01-20 22:33:15 -0600

    string test

--- hello.sh
+++ hello.sh
@@ -15,5 +15,20 @@ Presents=10
 BIRTHDAY=`date -d "$BIRTHDATE" +%A`
 
 echo $BIRTHDAY
+
+arrays=(1 2 "3" 4 five)
+echo ${#arrays[@]}
+
+# Code to extract the First name from the data record
+DATARECORD="last=Clifford,first=Johnny Boy,state=CA"
+COMMA1=`expr index "$DATARECORD" ','`  # 14 position of first comma
+CHOP1FIELD=${DATARECORD:$COMMA1}       #
+COMMA2=`expr index "$CHOP1FIELD" ','`
+LENGTH=`expr $COMMA2 - 6 - 1`
+FIRSTNAME=${CHOP1FIELD:6:$LENGTH}      # Johnny Boy
+echo $FIRSTNAME
+
+STRING="to be or not to be"
+echo ${STRING[@]/be/eat}        # to eat or not to be
 ############################################
 echo
