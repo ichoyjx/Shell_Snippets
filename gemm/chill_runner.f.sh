615bb5018f8d3a4513fade6f7de395b50d7d8e12
Brian Yang <brianyang1106@gmail.com>
2014-02-11 01:28:15 -0600

    start to go

--- gemm/chill_runner.f.sh
+++ gemm/chill_runner.f.sh
@@ -1,24 +1,7 @@
 #!/bin/sh
-#
-# Copyright 2003-2013 Jeffrey K. Hollingsworth
-#
-# This file is part of Active Harmony.
-#
-# Active Harmony is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Active Harmony is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with Active Harmony.  If not, see <http://www.gnu.org/licenses/>.
-#
+# This is the general process of using CHiLL
 scc -V -.spd gemm.f
 porky -forward-prop gemm.spd gemm.sp1
 porky -dead-code gemm.sp1 gemm.sp2
-./chill gemm.script
+chill gemm.script
 s2f gemm.lxf > gemm.modified.f 
