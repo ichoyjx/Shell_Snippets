aa156ffbacb1747a9aaea25ae41c519b8d1f6aff
Brian Yang <brianyang1106@gmail.com>
2015-04-23 02:02:17 -0700

    upgraded version

--- quote.bash
+++ quote.bash
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-TEMP="bin/runtest -suite hello -f fast -test abe bcd cde"
-TEMP2="bin/runtest -suite hello -f fast"
+TEMP='bin/runtest -suite hello -f "fast" -test abe bcd cde -copysrc'
+#TEMP='bin/runtest -suite hello -f "fast"'
 
-echo "$TEMP" | awk '
+FILENAME=$1
+
+if [[ $TEMP == *"-test"* ]]; then
+    LEN=`echo "$TEMP" | awk '
 {
   lenc=split($0, cmd, "-")
   for (i=1; i<=lenc; i++) {
@@ -12,19 +15,52 @@ echo "$TEMP" | awk '
     }
   }
 
+  print lens
+}'`
+elif [[ $TEMP == *"-suite"* ]]; then
+    LEN=0
+else
+    echo "NO SUITENAME"
+    exit 1
+fi
+
+MAIN=`echo "$TEMP" | awk -v lens="$LEN" '
+{
+  pos = 0
   for (i=1; i<=NF; i++) {
     if ($i == "-test")
       pos = i
   }
 
-  for (j=2; j<=lens; j++) {
-    for (i=1; i<=NF; i++) {
-      if (i<pos || i>=(pos+lens))
-        printf("%s ", $i)
-    }
+  for (i=1; i<=NF; i++) {
+    if (i<pos || i>=(pos+lens))
+      printf("%s ", $i)
+  }
+}'`
 
+
+if [[ $TEMP == *"-test"* ]]; then
+    echo "$TEMP" | awk -v mainstr="$MAIN" '
+{
+  lenc=split($0, cmd, "-")
+  for (i=1; i<=lenc; i++) {
+    if (index(cmd[i], "test") == 1) {
+      lens=split(cmd[i], teststr, " ")
+    }
+  }
+
+  for (j=2; j<=lens; j++) {
+    printf("%s", mainstr)
     printf("-test %s\n", teststr[j])
   }
   
 }'
-
+elif [[ $TEMP == *"-suite"* ]]; then
+    awk -v mainstr="$MAIN" '
+    {
+      for (i=1; i<=NF; i++) {
+        printf("%s", mainstr)
+        printf("-test %s\n", $i)
+      }
+    }' "$FILENAME"
+fi
